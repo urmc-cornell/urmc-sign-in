eeae0c1e024de51e00fba52cf81d4f68a2e988fa
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-12-05 13:26:38 -0500

    started working on sign in feature

--- backend/view_model.py
+++ backend/view_model.py
@@ -2,10 +2,8 @@ import gspread
 
 # GSpread Info
 sa = gspread.service_account(filename="service_account.json")
-
 sh = sa.open("URMC-Point-Tracking-SP24")
-
-wks = sh.worksheet("Points")
+points_sheet = sh.worksheet("Points")
 
 
 def create_event(title: str, time:str, date:str):
@@ -13,3 +11,35 @@ def create_event(title: str, time:str, date:str):
     event_worksheet.update('A1', "Attendees")
     event_worksheet.update('B1', f"Date: {date}")
     event_worksheet.update('C1', f"Time: {time}")
+
+def sign_in(name: str, netid: str, points_to_add: int):
+
+    # MARK: Updating Points Section
+
+    # Try to find netid in sheet
+    try:
+        position = points_sheet.find(netid).row
+
+    # User DNE in Sheet
+    except:
+        # TODO: binary search for next empty cell to add next person
+        # add_location = 
+        points_sheet.update(f'A{add_location}', name)
+        points_sheet.update(f'B{add_location}', netid)
+        points_sheet.update(f'C{add_location}', points_to_add)
+        print("New Attendee Added")
+
+    # Person in sheet
+    else:
+        # User exists in sheet
+        if position != None:
+            # Add points to add to users current points
+            curr_value = points_sheet.acell(f'C{position}').value 
+            new_value = int(curr_value) + points_to_add
+            # Update points cel to be this new value
+            points_sheet.update(f'C{position}', new_value)
+            print(f"Updated")
+
+
+    # MARK: Event Sign In
+    # TODO: Implement updating corresponding event attendees list
